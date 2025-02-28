0719ba1c91df5ebd94330075de3f62bff906e851
rohanprichard <rohanprichard@gmail.com>
2025-02-28 11:40:12 +0000

    adding assets and onboarding json schema

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ import fastapi
 import logging
 from .chat.server import router as chat_router
 from .auth.server import router as auth_router
-
+from fastapi.staticfiles import StaticFiles
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,6 +17,7 @@ app = fastapi.FastAPI()
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
+app.mount("/assets", StaticFiles(directory="backend/assets"), name="assets")
 
 @app.get("/health", tags=["health"])
 def read_root():
